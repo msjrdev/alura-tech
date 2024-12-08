51ebb0d8f2182bb2185694df06562035fa7d4562
Manoel Silva Junior <msilvajrdev@gmail.com>
2024-12-07 23:55:13 -0300

    Configurado minhas variáveis de ambiente

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d%jrbb&to-=29)e@sb-s!97olw9pqswrh0cbn)yjviy4=bfujs'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -143,8 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SOCIALACCOUNT_PROVIDERS = {
     'github': {
         'APP': {
-            'client_id': 'Ov23liiBl8G0O4JhlUIP',
-            'secret': '5a7afe6de36282cc3b3f384e7e70c6f36751aaf0',
+            'client_id': str(os.getenv('client_id')),
+            'secret': str(os.getenv('secret')),
             'key': ''
         }
     }
